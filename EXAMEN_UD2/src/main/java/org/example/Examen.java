7361c7ff1e2a05780ee35d7f3eec4131e790459f
A722\1dam <David-RD@outlook.es>
2024-10-31 16:33:19 +0100

    Ejercicio 2

--- EXAMEN_UD2/src/main/java/org/example/Examen.java
+++ EXAMEN_UD2/src/main/java/org/example/Examen.java
@@ -1,11 +1,30 @@
 package org.example;
+import java.time.LocalDateTime;
+import java.util.Scanner;
 public class Examen {
     public static void ejercicio1() {
-        
+
     }
 
     public static void ejercicio2() {
+        Scanner sc = new Scanner(System.in);
+        LocalDateTime hoy = LocalDateTime.now();
+        int anyoActual = hoy.getYear();
+        int edadPasada = -1;
 
+        System.out.print("Dime tu año de nacimiento: ");
+        int anyo = sc.nextInt();
+
+        if (anyo >= 1900 && anyo <= anyoActual){
+            for (int anyoPasado = anyo; anyo <= anyoActual; anyoPasado++){
+                edadPasada++;
+                anyo++;
+                System.out.println(anyoPasado + " - edad: " + edadPasada);
+                System.out.println();
+            }
+        } else {
+            System.out.println("Error: Debes ingresar una fecha mayor a 1900 o menor al año actual.");
+        }
     }
 
     public static void main(String[] args) {
