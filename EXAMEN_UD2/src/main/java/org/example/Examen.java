5a6d5817a8e4998b5c13f1a34609da7a88455300
A722\1dam <David-RD@outlook.es>
2024-10-31 15:51:49 +0100

    Inicio

---
+++ EXAMEN_UD2/src/main/java/org/example/Examen.java
@@ -0,0 +1,15 @@
+package org.example;
+public class Examen {
+    public static void ejercicio1() {
+        
+    }
+
+    public static void ejercicio2() {
+
+    }
+
+    public static void main(String[] args) {
+        ejercicio1();
+        ejercicio2();
+    }
+}
